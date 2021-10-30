12a9500cc7a0d65c3ee9b171fa7b700555586fba
kechkarayoub <kechkarayoub@gmail.com>
2021-10-30 19:41:39 +0100

    add data.test.js (terms_of_service/)

--- healthkools/src/components/terms_of_service/data.js
+++ healthkools/src/components/terms_of_service/data.js
@@ -1,4 +1,5 @@
 import {get} from "../../services/storage";
+const code_mode = process.env.REACT_APP_CODE_MODE;
 export const get_data = () => {
     return {
         company_address: "",
@@ -8,6 +9,6 @@ export const get_data = () => {
         responsable_address: "",
         responsable_full_name: "",
         site_name: get("general_information") && get("general_information").site_name ? get("general_information").site_name : "HealthKools",
-        site_url: "http://localhost:3000",
+        site_url: code_mode === "prod" ? "" : code_mode === "preprod" ? "" : code_mode === "server_dev" ? "" : "http://localhost:3000",
     };
 }
