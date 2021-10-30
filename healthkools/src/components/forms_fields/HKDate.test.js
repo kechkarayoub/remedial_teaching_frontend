c2df78cc09a2b2d538d2a817c1be5eeb22bb34da
kechkarayoub <kechkarayoub@gmail.com>
2021-10-30 18:41:02 +0100

    edit HKDate.test.js

--- healthkools/src/components/forms_fields/HKDate.test.js
+++ healthkools/src/components/forms_fields/HKDate.test.js
@@ -26,5 +26,24 @@ describe('HKDate component', () => {
         expect(label.textContent).toBe('Label test');
         expect(input.value).toBe(moment().format("DD/MM/YYYY"));
         expect(input.placeholder).toBe('Placeholder test');
+    });test('Should contains props values (disabled)', async () => {
+        render(<HKDate placeholder={"Placeholder test"}  value={moment().toDate()} disabled={true}/>);
+        const input = screen.getByPlaceholderText('Placeholder test');
+        expect(input.disabled).toBe(true);
+    });
+    test('Should contains props values (invalid_message)', async () => {
+        render(<HKDate value={moment().toDate()}  invalid_message={"Invalid message"}/>);
+        var invalid_message = screen.getByText('Invalid message');
+        expect(invalid_message.textContent).toBe("Invalid message");
+    });
+    test('Should contains props values (error_message)', async () => {
+        render(<HKDate value={moment().toDate()}  error_message={"Error message"}  invalid_message={"Invalid message"}/>);
+        var error_message = screen.getByText('Error message');
+        expect(error_message.textContent).toBe("Error message");
+    });
+    test('Should contains props values (valid_message)', async () => {
+        render(<HKDate value={moment().toDate()}  valid_message={"Valid message"} />);
+        var valid_message = screen.getByText('Valid message');
+        expect(valid_message.textContent).toBe("Valid message");
     });
 });
