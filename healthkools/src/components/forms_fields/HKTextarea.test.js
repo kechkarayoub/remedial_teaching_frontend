4edd1077f48416222d43b810cbc6c0b27ccf4b0d
kechkarayoub <kechkarayoub@gmail.com>
2021-10-30 18:51:24 +0100

    edit HKTextarea.test.js

--- healthkools/src/components/forms_fields/HKTextarea.test.js
+++ healthkools/src/components/forms_fields/HKTextarea.test.js
@@ -21,4 +21,24 @@ describe('HKTextarea component', () => {
         expect(textarea.value).toBe('address');
         expect(textarea.rows).toBe(3);
     });
+    test('Should contains props values (disabled)', async () => {
+        render(<HKTextarea placeholder={"Placeholder test"}  value={""} disabled={true}/>);
+        const textarea = screen.getByTestId('textarea');
+        expect(textarea.disabled).toBe(true);
+    });
+    test('Should contains props values (invalid_message)', async () => {
+        render(<HKTextarea value={""}  invalid_message={"Invalid message"}/>);
+        var invalid_message = screen.getByText('Invalid message');
+        expect(invalid_message.textContent).toBe("Invalid message");
+    });
+    test('Should contains props values (error_message)', async () => {
+        render(<HKTextarea value={""}  error_message={"Error message"}  invalid_message={"Invalid message"}/>);
+        var error_message = screen.getByText('Error message');
+        expect(error_message.textContent).toBe("Error message");
+    });
+    test('Should contains props values (valid_message)', async () => {
+        render(<HKTextarea value={""}  valid_message={"Valid message"} />);
+        var valid_message = screen.getByText('Valid message');
+        expect(valid_message.textContent).toBe("Valid message");
+    });
 });
