1a43a7befb7d3e1f00386e16269204b3c41ec97d
kechkarayoub <kechkarayoub@gmail.com>
2022-07-23 14:01:10 +0100

    add tests

---
+++ healthkools/src/components/_ressources.test.js
@@ -0,0 +1,10 @@
+import {images, flags} from "./_resources";
+ 
+describe('src/components/_ressources.js test', () => {
+    test('Images object should have 1 item', async () => {
+        expect(Object.keys(images).length).toBe(1);
+    });
+    test('Flags object should have 3 items', async () => {
+        expect(Object.keys(flags).length).toBe(3);
+    });
+});
---
+++ healthkools/src/pages/home/_ressources.test.js
@@ -0,0 +1,7 @@
+import {images} from "./_resources";
+ 
+describe('src/components/_ressources.js test', () => {
+    test('Images object should have 3 items', async () => {
+        expect(Object.keys(images).length).toBe(3);
+    });
+});
