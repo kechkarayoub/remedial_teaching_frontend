ce167d194d5b13cf971d4afdd92e4099c6b89d21
kechkarayoub <kechkarayoub@gmail.com>
2023-02-19 20:46:59 +0100

    add initials color component

---
+++ healthkools/src/components/InitialsColor.js
@@ -0,0 +1,77 @@
+import PropTypes from 'prop-types';
+import React, {Component} from "react";
+import styles from "styled-components";
+import { withTranslation } from 'react-i18next';
+class InitialsColor extends Component {
+  constructor(props){
+    super(props);
+  }
+  static defaultProps = {
+    added_className: "",
+    bg_color: "gray",
+    containerStyle: null,
+    initials: "",
+    on_click: null,
+    style: null,
+    title: "",
+    test_id: "initials_color_test_id",
+    t: val => val,
+  };
+  render(){
+    var style = this.props.style || {};
+    style.backgroundColor = this.props.bg_color;
+    if(this.props.on_click){
+      style.cursor = "pointer";
+    }
+    return (
+      <InitialsColorStyle className={"initials_bg_color_container " + this.props.added_className} style={this.props.containerStyle || {}} >
+        <div
+          className="initials_content"
+          style={style}
+          title={this.props.title}
+          data-testid={this.props.test_id}
+          onClick={evt => {
+            if(this.props.on_click){
+                this.props.on_click(evt);
+            }
+          }}
+        >
+          {this.props.initials}
+        </div>
+      </InitialsColorStyle>
+    );
+  }
+};
+const InitialsColorStyle = styles.div`
+  margin: auto;
+  height: 120px;
+  width: 120px;
+  .initials_content{
+    border-radius: 50%;
+    color: white;
+    font-size: 2.2em;
+    font-weight: bold;
+    height: 110px;
+    line-height: 110px;
+    margin: auto;
+    text-align: center;
+    vertical-align: middle;
+    width: 110px;
+  }
+`;
+
+InitialsColor.propTypes = {
+  added_className: PropTypes.string,
+  bg_color: PropTypes.string,
+  containerStyle: PropTypes.object,
+  initials: PropTypes.string,
+  on_click: PropTypes.oneOfType([
+      PropTypes.func,
+      PropTypes.object,
+  ]),
+  style: PropTypes.object,
+  title: PropTypes.string,
+  test_id: PropTypes.string,
+  t: PropTypes.func,
+};
+export default withTranslation('translations')(InitialsColor);
---
+++ healthkools/src/components/InitialsColor.test.js
@@ -0,0 +1,32 @@
+import InitialsColor from "components/InitialsColor";
+import { fireEvent, render, screen } from '@testing-library/react';
+import i18next from 'i18n_init';
+ 
+describe('InitialsColor component', () => {
+    test('Should render without crash', async () => {
+        render(<InitialsColor i18n={i18next}/>);
+    });
+    test('Should contains initials', async () => {
+        render(<InitialsColor initials={"IN"} i18n={i18next}/>);
+        const initials = screen.getAllByText('IN');
+        expect(initials.length).toBe(1);
+    });
+    test('Should contains title', async () => {
+        render(<InitialsColor title={"Initials color"} i18n={i18next}/>);
+        const initials = screen.getAllByTitle("Initials color");
+        expect(initials.length).toBe(1);
+    });
+    test('Should contains test_id', async () => {
+        render(<InitialsColor test_id={"test_id"} i18n={i18next}/>);
+        const initials = screen.getAllByTestId("test_id");
+        expect(initials.length).toBe(1);
+    });
+    test('On_click should called on click', async () => {
+        const on_click = jest.fn();
+        render(<InitialsColor initials={"IN"} i18n={i18next} on_click={on_click}/>);
+        const initials = screen.getByText('IN');
+        expect(on_click).toHaveBeenCalledTimes(0);
+        fireEvent.click(initials, {target: {}});
+        expect(on_click).toHaveBeenCalledTimes(1);
+    });
+});
